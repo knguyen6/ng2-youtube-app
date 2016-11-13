1c2efefb63dd265fc76c3be434652dc2e7ab2a94
Inno Estrera <inno.irving.estrera@gmail.com>
2016-11-12 18:02:36 -0800

    removed unused code

--- src/app/channel/channel.component.ts
+++ src/app/channel/channel.component.ts
@@ -1,31 +1,19 @@
 import { Component, OnInit, Input } from '@angular/core';
-import { YoutubeService } from "../youtube.service";
 
 @Component({
   selector: 'app-channel',
   templateUrl: './channel.component.html',
-  styleUrls: ['./channel.component.css'],
-  providers: [ YoutubeService ]
+  styleUrls: ['./channel.component.css']
 })
 export class ChannelComponent implements OnInit {
   @Input() channelResults: Array<any>;
+
   errorMessage: any;
 
-  constructor(private service: YoutubeService) {}
+  constructor() {}
 
   ngOnInit(): void {
     this.channelResults = [];
-  }
-
-  onKey(event, channelId) {
-    if(event.keyCode == 13){
-      this.service
-        .viewChannel(channelId)
-        .subscribe(
-          response => this.channelResults = response.items,
-          error => this.errorMessage = error
-        );
-    }
   }
 
 }
--- src/app/video/video.component.ts
+++ src/app/video/video.component.ts
@@ -1,31 +1,16 @@
 import { Component, OnInit, Input } from '@angular/core';
-import { YoutubeService } from "../youtube.service";
 
 @Component({
   selector: 'app-video',
   templateUrl: './video.component.html',
   styleUrls: ['./video.component.css'],
-  providers: [YoutubeService]
 })
 export class VideoComponent implements OnInit {
   @Input() videoDetail: Array<any>;
   errorMessage: any;
-  constructor(private service: YoutubeService) {}
+  constructor() {}
 
-    ngOnInit(): void {
-        this.videoDetail = [];
-    }
-
-    searchVideo(event, videoId): void {
-        if(event.keyCode == 13){
-            this.service.viewVideo(videoId)
-            .subscribe(
-              (response) => {
-                  console.log(response.items)
-                  this.videoDetail = response.items;
-              },
-              (error) => { this.errorMessage = error }
-          );
-        }
-    }
+  ngOnInit(): void {
+      this.videoDetail = [];
+  }
 }
